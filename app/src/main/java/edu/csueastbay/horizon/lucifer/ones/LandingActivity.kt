d343fca1fdffcaaf1e833101e4ba4df57cdf0cac
JOESEIDON <joemarsili@me.com>
2018-04-19 14:19:57 -0700

    Initial commit

---
+++ app/src/main/java/edu/csueastbay/horizon/lucifer/ones/LandingActivity.kt
@@ -0,0 +1,18 @@
+package edu.csueastbay.horizon.lucifer.ones
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.EditText
+import android.content.Intent
+
+
+
+class LandingActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_landing)
+  }
+
+}
