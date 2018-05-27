1d7e346b6ada0b3c412a8e03e207c151b8df7e41
Joseph Marsili <joemarsili@me.com>
2018-05-27 16:05:23 -0700

    final changes, this runs, emulator may need google play services update

--- app/src/main/java/edu/csueastbay/horizon/lucifer/ones/SplashActivity.kt
+++ app/src/main/java/edu/csueastbay/horizon/lucifer/ones/SplashActivity.kt
@@ -2,6 +2,7 @@ package edu.csueastbay.horizon.lucifer.ones
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.startActivity
 
@@ -9,7 +10,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        FirebaseApp.initializeApp(this);
     if(FirebaseAuth.getInstance().currentUser == null)
         startActivity<SignInActivity>()
     else
